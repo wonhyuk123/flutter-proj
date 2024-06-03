a40abfc171f097180c824698373968e2204c9698
rntaos77 <rntaos77@gmail.com>
2024-06-03 15:17:30 +0900

    final_weather_app

---
+++ weather_app/lib/screens/loading.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/data/my_location.dart';
+import 'package:weather_app/data/network.dart';
+import 'package:weather_app/screens/weather_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apikey = '938dc4b8f37cab860c17f6e7c5717481';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  double? latitude3;
+  double? longitude3;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    MyLocation myLocation = MyLocation();
+    await myLocation.getMyCurrentLocation();
+    latitude3 = myLocation.latitude2;
+    longitude3 = myLocation.longitude2;
+    print(latitude3);
+    print(longitude3);
+
+    Network network = Network(
+        'https://api.openweathermap.org/data/2.5/weather?'
+            'lat=$latitude3&lon=$longitude3&appid=$apikey&units=metric',
+        'https://api.openweathermap.org/data/2.5/air_pollution'
+            '?lat=$latitude3&lon=$longitude3&appid=$apikey');
+
+    var weatherData = await network.getJsonData();
+    print(weatherData);
+
+    var airData = await network.getAirData();
+    print(airData);
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return WeatherScreen(
+        parseWeatherData: weatherData,
+        parseAirPollution: airData,
+      );
+    }));
+  }
+
+  void fetchData() async {
+    //   var myJson = parsingData['weather'][0]['description'];
+    //   print(myJson);
+    // }else{
+    //   print(response.statusCode);
+    // }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
